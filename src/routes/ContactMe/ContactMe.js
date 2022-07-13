4d317ab8432be68c28de29d21a018fcb35aeb5fd
AbdurasulDj <abdurasulac@gmail.com>
2022-07-13 13:49:25 +0500

    responsivness done

--- src/routes/ContactMe/ContactMe.js
+++ src/routes/ContactMe/ContactMe.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import emailjs from '@emailjs/browser';
 import './ContactMe.scss'
 
 export default function ContactMe() {
   const [data, setData] = React.useState({
-    name: '',
-    email: '',
+    user_name: '',
+    user_email: '',
     message: ''
   })
 
@@ -17,9 +18,18 @@ export default function ContactMe() {
     })
   }
 
-  function handleSubmit(event) {
-    event.preventDefault()
-  }
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_q1ticzm', 'template_twkixn9', form.current, 'x0jRGgktNpxClXGtk')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
 
   return (
     <div className="contactme-container" id='contactMe'>
@@ -31,9 +41,9 @@ export default function ContactMe() {
             <span></span>
           </div>
         </div>
-        <form onSubmit={handleSubmit}>
-          <input type="text" name="name" onChange={handleChange} placeholder="Your name" value={data.name}/>
-          <input type="email" name="email" onChange={handleChange} placeholder="Your email" value={data.email} />
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="user_name" onChange={handleChange} placeholder="Your name" value={data.name}/>
+          <input type="email" name="user_email" onChange={handleChange} placeholder="Your email" value={data.email} />
           <textarea name="message" onChange={handleChange} placeholder="Your message" value={data.message}/>
           <button id='submit'>Send</button>
         </form>
